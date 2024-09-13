adde53a43d394b8d163fc46027a8a4349bbdad20
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-09-14 01:12:50 +0900

    First.js

---
+++ JavaScript-Course/First.js
@@ -0,0 +1,9 @@
+// console.log('Hell world')
+// console.log(5  + 6 -4 / 2 * 4);
+
+let firstName = "Rao";
+let lastName = "Hammad";
+let age = 22;
+
+let userIntro = "Hi, my name is " + firstName  + "  " + lastName  + age + "I am Student of computer science."
+console.log(userIntro);
