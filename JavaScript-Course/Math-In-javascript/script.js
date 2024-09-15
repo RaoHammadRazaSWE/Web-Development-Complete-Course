5b797daf476be59f59c8f535275616083f30d0f7
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-09-15 11:12:18 +0500

    Add files via upload

---
+++ JavaScript-Course/Math-In-javascript/script.js
@@ -0,0 +1,7 @@
+const width = prompt ('Enter Rectangle width')
+const height = prompt ('Enter Rectangle Hight')
+const Area = width * height
+console.log('Area of Regtangle is ', Area)
+
+const andResult = '' && 'Hello'
+console.log(andResult)
