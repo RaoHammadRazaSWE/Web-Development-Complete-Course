62375f82f5c4d53204189cd55ccc76da09c8a431
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-09-16 06:59:10 +0500

    Add files via upload

---
+++ JavaScript-Course/Ternary-Operator/script.js
@@ -0,0 +1,14 @@
+// const username = 5 > 2 ? 'Rao': 'Ham'
+
+// console.log(username)
+
+// const gender = 'F'
+
+// const userMessage = `${gender === 'F' ? 'She' : 'He'} is a College student.`
+
+// console.log(userMessage)
+
+
+const result = 5 ? 'Ali' : 'Rao' ? '12' : 0
+
+console.log(result)
