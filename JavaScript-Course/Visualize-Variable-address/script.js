47d535c3abcf1f3b90745facfbb04999f3241635
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-09-21 22:07:38 +0500

    Add files via upload

---
+++ JavaScript-Course/Visualize-Variable-address/script.js
@@ -0,0 +1,9 @@
+const firstName = 'Rao';
+const userAge = 14;
+const isGraduate = false;
+const birthYear = 2001
+const hasjob = true
+const  myNull = null
+const myUndefined = undefined
+const myEmptyString = ''
+// console.log(firstName)
