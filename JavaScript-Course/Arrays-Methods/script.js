f93dbf73bb3283093ddce9009ee242fef24c06f8
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-09-25 11:25:57 +0500

    Add files via upload

---
+++ JavaScript-Course/Arrays-Methods/script.js
@@ -0,0 +1,15 @@
+const evenNumbers = [0, 2, 4, 6]
+const animals = ['Dog', 'Cat', 'Rat', 'Lion', 'Cow']
+
+// EvenNumbers.shift()
+// EvenNumbers.unshift() // This will add an element at the beginning
+
+// const addedArray1 =  evenNumber.concat(animals)
+
+// evenNumbers.indexof(2)
+
+// const elementIndex =  animals.includes('Cat')
+
+// Splice
+
+// animals.splice(2,1)
