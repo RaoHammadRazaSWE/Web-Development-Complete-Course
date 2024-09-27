13a4c5b715f52efd91f228c104c866a281e6b796
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-09-27 09:09:38 +0500

    Add files via upload

---
+++ JavaScript-Course/Objects-and-Arrays/script.js
@@ -0,0 +1,41 @@
+const fruits = ['Mango', 'Apple', 'Orange']
+
+// const myFruits = fruits
+
+// myFruits.push('Dates')
+// myFruits.push('Grapes')
+
+// const myFruits = [...fruits]
+// Object.assign(myFruits, fruits)
+
+// const myFruits = [].concat(fruits)
+
+// const myFruits = fruits.slice()
+
+const user1 = {
+    firstName: 'Rao',
+    lastName: 'Raza',
+    pata: {
+        city: 'Multan',
+        pincode: 989898,
+    },
+    subject: ['Physics','Urdu','English']
+}
+
+ // Shallow copy
+const user2 = {...user1}
+
+// const user2 = {...user1}
+
+// const user2 = user1
+
+// user2.lastName = 'Hammad'
+// user1.firstName = 'Ali'
+
+
+// const username1 = 'Rao'
+
+// let username2 = 'username1'
+
+// username2 = username2  + 'Hammad Raza'
+
