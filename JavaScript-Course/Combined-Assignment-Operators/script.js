cd4e63319d85312cca021ade0b4f7c984b73a760
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-09-28 11:12:54 +0500

    Add files via upload

---
+++ JavaScript-Course/Combined-Assignment-Operators/script.js
@@ -0,0 +1,29 @@
+let num  = 5;
+
+// num = num + 2;
+
+// += Addition Assignment Operator.
+
+// num += 5;
+
+// num = num * 2
+// num = num * 2
+// num = num * 2
+
+// Multiplication Assignment operator
+
+// num *= 2;
+// num *= 2;
+// num *= 2;
+
+// Increament and Decrenebt OP
+
+// const newNum = ++num;
+// const newNum = ++num
+// const newNum = --num
+
+// const newNum = num++
+// const newNum = num--
+
+// num = num + 1
+// num += 1
