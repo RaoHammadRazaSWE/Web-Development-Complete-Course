c45ecc9ab122f0ec32d2e5e65549e80df8eaca7b
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-10-01 20:58:38 +0500

    Add files via upload

---
+++ JavaScript-Course/While-Loop/script.js
@@ -0,0 +1,19 @@
+console.log('Program Started')
+
+// let num = 0;
+
+// while(num <= 10) {
+//     console.log(num);
+//     // num = num +1 ;
+//     num++;
+// } 
+
+const friends = ['Rao', 'Ali', 'Hammad', 'Raza', 'Arif']
+
+let i = 0;
+while( i < 5){
+    console.log(friends[i]);
+    i++;
+
+}
+console.log('Program Ended')
