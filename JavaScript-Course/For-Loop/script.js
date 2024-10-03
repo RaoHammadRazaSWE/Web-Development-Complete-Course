f94f61ab9a2ffe932825475ffede3d74e2acf29c
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-10-03 21:56:57 +0500

    Add files via upload

---
+++ JavaScript-Course/For-Loop/script.js
@@ -0,0 +1,18 @@
+console.log('Program Start')
+
+// const friends = ['Rao', 'Ali', 'Hammad', 'Raza', 'Arif']
+
+
+// for(let i = 0; i < friends.length; i++){
+//     console.log(`${i + 1}. ${friends[i]}`);
+//     friends[i] = friends[i] + 'Coder';
+
+// }
+
+for(let i = 0; i < 10; i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+console.log('Program Ended')
