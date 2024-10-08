2eba9714ef1c49a339c52498c63404b21e336579
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-10-08 09:29:06 +0500

    Add files via upload

---
+++ JavaScript-Course/Scope-Global&Local/script.js
@@ -0,0 +1,26 @@
+// debugger
+const username = 'Raza'
+const userAge = 23
+
+var a = 50
+
+function add() {
+    debugger
+    const username = 'Akash'
+    const x = 5
+    const y = 8
+    console.log(x + y)
+    console.log(username)
+    }
+
+function subtract() {
+    const x = 15
+    const y = 18
+    console.log(x - y)
+    console.log(username)
+}
+
+add()
+subtract()
+
+console.log('Program Ended')
