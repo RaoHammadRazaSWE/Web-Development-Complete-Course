a41b89e7761f8157d1be486f6d05b01ff50f9166
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-10-09 09:09:08 +0500

    Add files via upload

---
+++ JavaScript-Course/Call-Stack/script.js
@@ -0,0 +1,26 @@
+// sayhi()
+
+// const username = 'Rao';
+// const userAge = 22;
+
+// function sayhi(){
+//     const a = 14;
+//     const b = 12;
+//     add(4, 5)
+// }
+
+// function add(x, y){
+//     something()
+//     return x + y
+// }
+
+// function something(){
+//     console.log('something')
+// }
+
+function introductionMe() {
+    console.log('Hello, my name is Rao and  I am a 22 years old.')
+}
+
+introductionMe()
+console.log('Program Ended')
