51db0267a88b1b2794fa8ecc0a3390b56f6925f8
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-10-12 10:08:16 +0500

    Add files via upload

---
+++ JavaScript-Course/Difference-between-Methods-and-Functions/script.js
@@ -0,0 +1,16 @@
+const maths = {
+    E: 2.71828182,
+    add: function (a, b) {
+        return a + b
+    },
+    square: function(num) {
+        return  num * num
+    },
+    subtract(a, b){
+        return a - b
+    },
+    cube(num) {
+        return num ** 3
+    }
+
+}
