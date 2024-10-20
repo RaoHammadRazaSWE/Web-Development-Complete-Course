71b55ddff38d0e268ee7e258d629c63b9a217f2c
Rao Hammad Raza <145425438+RaoHammadRazaSWE@users.noreply.github.com>
2024-10-20 09:48:45 +0500

    Add files via upload

---
+++ JavaScript-Course/Arrow-functions/script.js
@@ -0,0 +1,33 @@
+// Function Declaration
+
+// function square(num) {
+//     return num * num
+// }
+
+// Function Expression
+
+// const square = function(num) {
+//     return num * num
+// }
+
+// Arrow function Expression
+
+// const square = (num) => {
+    // Expley set return:
+//     return num * num
+// }
+
+// impley set return :
+
+// const square = num => num * num
+
+// const add =(a , b)=> a  + b
+
+const random = () => (
+    Math.floor(Math.random() * 10) + 1
+
+)
+
+setTimeout (() => {
+    console.log('Hello Coder');
+}, 2000)
