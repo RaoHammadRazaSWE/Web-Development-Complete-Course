e599ce8cef551300e828b293352ade78e7ac574c
Rao Hammad Raza <145425438+RaoHammadRazaSWE@users.noreply.github.com>
2024-10-24 09:49:35 +0500

    Add files via upload

---
+++ JavaScript-Course/Execution-Context/script.js
@@ -0,0 +1,17 @@
+sayhi()
+
+var username = 'Rao'
+var userAge = '22'
+
+function sayhi(){
+    const a = 14
+    const b = 13
+    console.log('Hi');
+    console.log(a, b)
+}
+
+function add(x, y) {
+    return x + y;
+}
+
+console.log('Program Ended')
