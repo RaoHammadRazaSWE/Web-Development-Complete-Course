e04178f4b1c419e3524a3cb55ca9aabb055cbbb9
Rao Hammad Raza <145425438+RaoHammadRazaSWE@users.noreply.github.com>
2024-11-01 10:48:42 +0500

    Add files via upload

---
+++ JavaScript-Course/For-Each-Array-Method/script.js
@@ -0,0 +1,26 @@
+const fruits = ['banana', 'apple', 'peach', 'mango', 'grapes']
+
+// For of  loop :
+
+// for(const fruit of fruits) {
+//     console.log(fruit);
+// }
+
+//  For Each Method: 
+
+// fruits.forEach
+
+// fruits.forEach(function(fruit){
+//     // console.log("Hello guys please fellow me Github account");
+//     console.log(fruit);
+// })
+
+fruits.forEach((fruit) => {
+    console.log(fruit.toUpperCase());
+})
+
+// function abc(el) {
+//     console.log(el);
+// }
+
+// fruits.forEach(abc)
