0f8787226ee0a225c25ba0b0acf9b06c0b37d534
Rao Hammad Raza <145425438+RaoHammadRazaSWE@users.noreply.github.com>
2024-11-10 23:10:52 +0500

    Add files via upload

---
+++ JavaScript-Course/How-to-styles-in-javascript/script.js
@@ -0,0 +1,11 @@
+const h1 = document.querySelector('h1')
+const paragraph = document.querySelector('p')
+
+// console.log(paragraph.innerHTML);
+
+// paragraph.innerHTML = '<h4>Hiii</h4>'
+
+// document.querySelector('h1').getAttribute('class')
+
+
+document.querySelector('h1').setAttribute('id', 'heading')
